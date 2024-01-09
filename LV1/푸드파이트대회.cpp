0ef383a74e39446bc1534d89d1c59c1523b073b7
Xrofe <70887592+keesung98@users.noreply.github.com>
2024-01-09 18:52:46 +0900

    Add files via upload

---
+++ LV1/푸드파이트대회.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+string solution(vector<int> food) {
+    string answer = "";
+    string temp = "";
+    for (int i = 1; i < food.size(); ++i) {
+        for (int j = 0; j < food[i] / 2; ++j) {
+            temp += (char)(i + 48);
+        }
+    }
+
+    answer += temp;
+    answer += (char)(0 + 48);
+    reverse(temp.begin(), temp.end());
+    answer += temp;
+    return answer;
+}
