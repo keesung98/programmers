1bb08e5c8d049310ff9f2789119b1e7ed488e058
Xrofe <70887592+keesung98@users.noreply.github.com>
2024-03-05 13:26:36 +0900

    Add files via upload

---
+++ LV2/방문길이.cpp
@@ -0,0 +1,49 @@
+#include <string>
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+int dx[4] = { 0, -1, 0, 1 };
+int dy[4] = { -1, 0, 1, 0 };
+
+int solution(string dirs) {
+    int answer = 0;
+    bool visited[11][11][4];
+    memset(visited, false, sizeof(visited));
+
+    int x = 5; 
+    int y = 5;
+
+    for (int i = 0; i < dirs.size(); i++) {
+        int dir = 0;
+        char a = dirs[i];
+        if (a == 'L') {
+            dir = 0;
+        }
+        else if (a == 'U') {
+            dir = 1;
+        }
+        else if (a == 'R') {
+            dir = 2;
+        }
+        else {
+            dir = 3;
+        }
+
+        int nx = x + dx[dir];
+        int ny = y + dy[dir];
+
+        if (nx < 0 || ny < 0 || nx >= 11 || ny >= 11) continue;
+        int countdir = (dir + 2) % 4;
+        if (!visited[x][y][dir] && !visited[nx][ny][countdir]) {
+            visited[x][y][dir] = true;
+            visited[nx][ny][countdir] = true;
+            answer++;
+        }
+        x = nx;
+        y = ny;
+    }
+
+    return answer;
+}
